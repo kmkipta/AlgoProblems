2501ff2820211b2238a6ec415f82bf8b0cd87e55
kmkipta <kmkipta@gmail.com>
2019-05-07 00:33:05 -0400

    Create BinaryInorderTreeTraversal.js

---
+++ BinaryInorderTreeTraversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ * DFS Traversal
+ */
+var inorderTraversal = function(root) {
+    let res = [];
+    let conS = [];
+    if (root == null)
+        return res;
+    let curr = root;
+    // conS.push(root);
+    while(conS.length != 0 || curr != null) {
+        while(curr != null) {
+            conS.push(curr);
+            curr = curr.left;
+        }
+        curr = conS.pop();
+        res.push(curr.val);
+        curr = curr.right;
+    }
+    return res;
+};
