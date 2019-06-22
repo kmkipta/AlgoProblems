d3cc8588b19432f94d8b04fdb443eeb62b4eef99
kmkipta <kmkipta@gmail.com>
2019-06-22 02:12:31 -0400

    Add files via upload

--- 204CountPrimes.py
+++ 204CountPrimes.py
@@ -1,16 +1,38 @@
-class Solution(object):
-    def countPrimes(self, n):
-        # Sieve of Eratosthenes
-        i = 2
-        cnt = 0
-        numMap = []
-        while i <= 10:
-            numMap.append(i)
-        
-        for item in numMap:
-            
+import math
 
+class Solution(object):
+    # empty contructor
+    def __init__(self):
+        self.resList = []
+        self.cnt = 0
+
+    def countPrimes(self, n):
+        cnt = 0
+        # list of quick hits
+        i = 1
+        numMap = {}
+        while i <= n:
+            numMap[i] = True
+            i += 1
+
+        # Sieve of Eratosthenes
+        i = 2 # start at 2
+        # loop over all numbers until i^2 > n
+        while i <= math.sqrt(n):
+            if numMap[i] is True:
+                # remove multiples of each number
+                j = 2
+                k = 0 # multiple counter
+                while j <= n:
+                    numMap[j] = False
+                    j = math.pow(i, 2) + k * i
+                    k += 1
+            i += 1
+        # iterate over keys, count all primes
+        for num in numMap:
+            if numMap[num] is True: 
+                cnt += 1
         return cnt
 
 sol = Solution()
-sol.countPrimes(10)
+print(sol.countPrimes(3))
