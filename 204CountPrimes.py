2e1d99bb9766b45f3ed684971e6c33cccdfde412
kmkipta <kmkipta@gmail.com>
2019-06-21 20:40:16 -0400

    Add files via upload
    saving sieve of eratosthenes

---
+++ 204CountPrimes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPrimes(self, n):
+        # Sieve of Eratosthenes
+        i = 2
+        cnt = 0
+        numMap = []
+        while i <= 10:
+            numMap.append(i)
+        
+        for item in numMap:
+            
+
+        return cnt
+
+sol = Solution()
+sol.countPrimes(10)
