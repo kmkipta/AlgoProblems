71dcd795cd3180f03fb15897f7c1f7e184543567
kmkipta <kmkipta@gmail.com>
2019-12-12 02:08:19 -0500

    Create 628ProdThreeNumbers.js

---
+++ 628ProdThreeNumbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    let max1 = Number.MAX_SAFE_INTEGER;
+    let max2 = Number.MAX_SAFE_INTEGER;
+    let max3 = Number.MAX_SAFE_INTEGER;
+    for(n of nums){
+        if(n)
+    }
+};
