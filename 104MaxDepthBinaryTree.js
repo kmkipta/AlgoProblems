6995e6a0d8ecdebfa54c3445fee67db3f683e7dc
kipta <kmkipta@gmail.com>
2020-02-21 16:06:51 -0500

    passing recursive tree test

--- 104MaxDepthBinaryTree.js
+++ 104MaxDepthBinaryTree.js
@@ -13,23 +13,22 @@ const maxDepth = (root) => {
     let count = 0
     if (root == null)
         return count
-    count++
-    if (root.left == null && root.right == null)
-        return count
-    
-    if (root.left != null)
-        recurseDir(root.left, count)
-    if (root.right != null)
-        recurseDir(root.right, count)
-    return count
+    return mdHelper(root, count + 1)
 };
 
-const recurseDir = (root, count) => {
-    count++
-    if (root.left == null && root.right == null)
-        return
-    if (root.left != null)
-        recurseDir(root.left, count)
-    if (root.right != null)
-        recurseDir(root.right, count)
+const mdHelper = (root, count) => {
+    if (root == null)
+        return 0
+    if (root.left == null && root.right  == null) {
+        return count
+    }
+    if (root.left && root.right) {
+        return Math.max(mdHelper(root.right, count + 1), mdHelper(root.left, count + 1))
+    }
+    else if (root.left != null) {
+        return mdHelper(root.left, count + 1)   
+    }
+    else if (root.right != null) {
+        return mdHelper(root.right, count + 1)
+    }
 }
