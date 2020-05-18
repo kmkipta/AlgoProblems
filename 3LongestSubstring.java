87a34a09d8da679b7a21adb63b62db5663e428dd
kmkipta <kmkipta@gmail.com>
2020-05-18 01:02:17 -0400

    Create 3LongestSubstring.java

---
+++ 3LongestSubstring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Scratch {
+    public static void main(String[] args) {
+
+    }
+
+    // sliding window
+    // check if duplicates, if not add new char
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        Set<Character> currentWord = new HashSet<>();
+        
+        while (i < n) {
+            
+        }
+    }
+
+}
